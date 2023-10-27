be2fc59368f8e442fe41357319f5037e4ed78200
fatihuzdilli <fatih.uzdilli@gmail.com>
2023-10-28 01:15:36 +0200

    Update List.vue: introduced productFilter url param

--- src/components/List.vue
+++ src/components/List.vue
@@ -226,20 +226,23 @@ export default {
       }
       return todoDesc.slice(0, -2);
     },
+    filteredItems: function () { // To filter items by product, pass a url param 'productName' matching the product.
+      return this.items.filter(item => item.product === this.productName);
+    },
     orderedItems: function () {
-      let ordered = _.orderBy(this.items, "creationTime");
+      let ordered = _.orderBy(this.filteredItems, "creationTime");
       return _.filter(ordered, (item) => item.status == null);
     },
     orderedDoneItems: function () {
-      let ordered = _.orderBy(this.items, "doneTime");
+      let ordered = _.orderBy(this.filteredItems, "doneTime");
       return _.filter(ordered, (item) => item.status === "DONE");
     },
     orderedDeletedItems: function () {
-      let ordered = _.orderBy(this.items, "deletedTime");
+      let ordered = _.orderBy(this.filteredItems, "deletedTime");
       return _.filter(ordered, (item) => item.status === "DELETED");
     },
     orderedArchivedItems: function () {
-      let ordered = _.orderBy(this.items, "archivedTime", "desc");
+      let ordered = _.orderBy(this.filteredItems, "archivedTime", "desc");
       return _.filter(ordered, (item) => item.status === "ARCHIVED");
     },
   },
@@ -306,6 +309,10 @@ export default {
         })
       );
     },
+  },
+  created() {
+    // Access the URL parameter and set it to this.productFilter. This is probably a hack. We should use vue router feature instead maybe.
+    this.productFilter = this.$route.params.productFilter || '';
   },
 };
 </script>
